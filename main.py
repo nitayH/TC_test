ec2c9864a7f21b20c52076fa5c487ea317b1e974
nitayH <nitay@redefine.dev>
2023-01-01 19:00:57 +0200

    trying push to main

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     print(f"project name: {os.environ['TEAMCITY_PROJECT_NAME']}\nversion: {os.environ['TEAMCITY_VERSION']}\nbuildconf: {os.environ['TEAMCITY_BUILDCONF_NAME']}\nID: {os.environ['BUILD_NUMBER']}")
     cwd = os.getcwd()
     print(f"cwd: {cwd}")
+    print("this is master test")
 
 
 
