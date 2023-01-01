84d946d3408fae22880e0102d688ce61679cf218
nitayH <nitay@redefine.dev>
2023-01-01 14:59:54 +0200

    trying stuff

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import os
+import subprocess
+
+def get_system_property(property: str):
+    result = subprocess.run(["echo", f"%{property}%"], capture_output=True, text=True)
+    return result.stdout
 
 def main():
     print(f"project name: {os.environ['TEAMCITY_PROJECT_NAME']}\nversion: {os.environ['TEAMCITY_VERSION']}\nbuildconf: {os.environ['TEAMCITY_BUILDCONF_NAME']}\nID: {os.environ['BUILD_NUMBER']}")
+    checkout_dir = get_system_property("teamcity.build.checkoutDir")
+    working_dir = get_system_property("teamcity.build.workingDir")
+    build_id = get_system_property("teamcity.build.id")
+    branch = get_system_property("teamcity.build.branch")
+    commit = get_system_property("system.build.vcs.number")
+    changed_files = get_system_property("teamcity.build.changedFiles.file")
+    print(f"checkout dir: {checkout_dir}\n working dir: {working_dir}\n build id: {build_id}\n branch: {branch}\ncommit:{commit}\nchanged files: {changed_files}")
+
+
+    
+    print()
+
+
 
 
 if __name__ == "__main__":
